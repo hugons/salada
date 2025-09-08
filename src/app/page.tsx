cef902e05bc20833b925644fbfc935491c95b8e9
hugons <hugo@lisbonlab.com>
2025-09-08 10:40:44 +0100

    Atualização da homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">New Salad</h1>
+      <h1 className="text-4xl font-bold mb-8">Salada de palavras</h1>
       <p className="text-lg mb-8">Jogo de palavras inspirado no Word Salad</p>
       <div className="flex gap-4">
         <Link href="/diario" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
