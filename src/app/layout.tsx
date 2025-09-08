60715beabc818bb447e619563607fe769cd459a0
hugons <hugo@lisbonlab.com>
2025-09-08 10:33:23 +0100

    feat: Add Roboto Google Font and apply to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
 import './globals.css'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'New Salad',
@@ -17,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-PT">
+    <html lang="pt-PT" className={roboto.className}>
       <body>
         {children}
         <script src="/register-sw.js" defer></script>
